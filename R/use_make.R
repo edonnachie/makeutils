07f332af615d37e2e1e8bc5cc5846a86a920714f
Ewan Donnachie <ewan@donnachie.net>
2018-11-30 02:28:34 +0100

    use_make copies to Makefile_template if Makefile exists

--- R/use_make.R
+++ R/use_make.R
@@ -2,12 +2,16 @@
 #'
 #' This function is inspired by the `usethis` package. It copies a Makefile
 #' template to the project root, containing generic rules for building R
-#' projects.
+#' projects. If a Makefile already exists, the template is created as
+#' `Makefile_template`.
+#'
 #' @return Output of file.copy, signifying success of operation
 #' @export
 use_make <- function() {
+  outfile <- here::here("Makefile")
+  if (file.exists(outfile)) outfile <- here::here("Makefile_template")
   file.copy(
     system.file("Makefile_generic.txt", package = "makeutils"),
-    here::here("Makefile")
+    outfile
   )
 }
